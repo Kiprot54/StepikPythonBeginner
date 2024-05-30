c88d1acc3819cea75ce8fcb586956b6c134aea84
Kiprot <KirKoLearn@gmail.com>
2024-05-30 20:12:45 +0300

    Решил задачу 2.5.3

---
+++ task2.5.3.Mandarins.py
@@ -0,0 +1,22 @@
+n, k = 0, 0
+while True:
+    n = input('Введи количество школьников: ')
+    if n.isdigit() and int(n) != 0:
+        print()
+        break
+    else:
+        print('Нужно ввести целое положительное число')
+        print()
+while True:
+    k = input('Введи количество мандарин: ')
+    if k.isdigit():
+        print()
+        break
+    else:
+        print('Нужно ввести целое положительное число')
+        print()
+
+mandarins_in_schoolboy = int(k) // int(n)
+mandarins_in_pottle = int(k) % int(n)
+print(f'Мандарин у каждого школьника: {mandarins_in_schoolboy}')
+print(f'Осталось мандарин в корзине: {mandarins_in_pottle}')
