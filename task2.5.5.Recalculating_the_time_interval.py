9370764799365986e4c2632deeeb0ae72e8d0530
Kiprot <KirKoLearn@gmail.com>
2024-06-01 15:37:03 +0300

    Решил задачу 2.5.5

---
+++ task2.5.5.Recalculating_the_time_interval.py
@@ -0,0 +1,14 @@
+print('Пересчёт временного интервала')
+print()
+while True:
+    minutes = input('Введи количество минут: ')  # 65
+    print()
+    if minutes.isdigit():
+        minutes = int(minutes)
+        hours = minutes // 60
+        minutes_left = minutes % 60
+        print(f'{minutes} мин - это {hours} час {minutes_left} минут.')
+        break
+    else:
+        print('Нужно ввести положительное целое число')
+        print()
