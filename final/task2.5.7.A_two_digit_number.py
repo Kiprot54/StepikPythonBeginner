ef2f55c2264f802baf984ecc45dfd52a4d8989a8
Alex Ko <help.mail015@gmail.com>
2024-06-04 13:53:51 +0300

    remarks

--- final/task2.5.7.A_two_digit_number.py
+++ final/task2.5.7.A_two_digit_number.py
@@ -2,13 +2,14 @@ print('Двузначное число')
 print()
 
 while True:
-    n = input('Введи число: ')
+    n = input('Введи целое двузначное число: ')
     if n.isdigit() and 10 <= int(n) <= 99:
         n = int(n)
         break
     else:
-        print('Нужно ввести целое число, большее 9 и меньшее 100')
+        print('Нужно ввести целое двузначное число')
         print()
 print()
+print(f'Число: {n}')
 print(f'Единицы: {n % 10}')
 print(f'Десятки: {n // 10}')
