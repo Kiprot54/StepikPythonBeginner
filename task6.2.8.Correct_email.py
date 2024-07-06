117b37989a4f9bb405ef7cd1fabd6b74dd0f6049
Kiprot54 <kirkolearn@gmail.com>
2024-07-06 14:54:52 +0300

    Решил задачи 6.2.5-6.2.8

---
+++ task6.2.8.Correct_email.py
@@ -0,0 +1,8 @@
+print('Корректный email')
+print()
+email = input('Введи емайл: ')
+
+if ('@' in email and '.' in email) and email.count('@') == 1:
+    print(f'Емайл <{email}> корректный')
+else:
+    print(f'В строке <{email}> не корректный')
