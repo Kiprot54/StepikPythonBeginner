30e845f1f963e2438dd98d1750c44ae7cfd95592
Alex Ko <help.mail015@gmail.com>
2024-07-08 14:13:36 +0300

    add check string for float

---
+++ ifnumber.py
@@ -0,0 +1,33 @@
+def if_number(n):
+    # Если возможное число отрицательное - избавляемся от минуса
+    if n[0] == '-':
+        n = n[1:]
+
+    # Подсчитываем количество точек в строке
+    count_dot = n.count('.')
+
+    if count_dot > 1:
+        return False
+    else:
+        if n.count('.') == 1:
+            index_dot = n.find('.')
+            if index_dot != len(n) - 1:
+                # Если в строке одна точка, и она не последний символ, делим эту строку на две: до точки и после
+                n1 = n[:index_dot]
+                n2 = n[index_dot + 1:]
+
+                # Проверяем, являются ли строки целыми числами
+                if n1.isdigit() and n2.isdigit():
+                    return 'float'
+                else:
+                    return False
+            else:
+                return False
+        else:
+            # Если в строке нет точек - проверяем, является ли строка целым числом
+            if n.isdigit():
+                return 'int'
+            else:
+                return False
+
+
---
+++ ifnumber_test.py
@@ -0,0 +1,15 @@
+import ifnumber
+
+while True:
+    num = input('Введи число: ')
+    if_number = ifnumber.if_number(num)
+    if if_number == 'int':
+        num = int(num)
+        break
+    elif if_number == 'float':
+        num = float(num)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести число')
+    print()
+print(num)
