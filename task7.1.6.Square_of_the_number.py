945edaaf37a38bb1f8802ddece7daa628cc2276f
Kiprot54 <KirKoLearn@gmail.com>
2024-07-18 13:40:24 +0300

    Решил задачу 7.1.6

---
+++ task7.1.6.Square_of_the_number.py
@@ -0,0 +1,15 @@
+import ifnumber
+print('Квадрат числа')
+print()
+while True:
+    num = input('Введи целое положительное число: ')
+    if_number = ifnumber.if_number(num)
+    if if_number == 'int' and int(num) > 0:
+        num = int(num)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
+    print()
+print()
+for i in range(num + 1):
+    print(f'Квадрат числа {i} равен {i ** 2}')
