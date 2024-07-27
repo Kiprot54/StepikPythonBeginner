f80ac1ca501fff9097aa77b148c496b5ea6caef0
Kiprot54 <KirKoLearn@gmail.com>
2024-07-27 22:28:03 +0300

    Решил задачу 7.3.2

---
+++ task7.3.2.Sum_of_numbers.py
@@ -0,0 +1,28 @@
+import ifnumber
+
+print('')
+print()
+
+while True:
+    n = input('Введи целое положительное число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) > 0:
+        n = int(n)
+        print()
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
+    print()
+total = 0
+for i in range(n):
+    while True:
+        m = input('Введи целое число: ')
+        if_number = ifnumber.if_number(m)
+        if if_number == 'int':
+            m = int(m)
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+    total += m
+print(f'Сумма введенных чисел равна {total}')
