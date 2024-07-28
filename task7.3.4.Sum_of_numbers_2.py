01ba9fa743f68faac2e4e1234a34b9343822112f
Kiprot54 <KirKoLearn@gmail.com>
2024-07-28 17:18:24 +0300

    Решил задачу 7.3.4

---
+++ task7.3.4.Sum_of_numbers_2.py
@@ -0,0 +1,21 @@
+import ifnumber
+
+print('Сумма чисел 2')
+print()
+
+while True:
+    n = input('Введи целое положительное число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int':
+        n = int(n)
+        print()
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
+    print()
+total = 0
+for i in range(1, n + 1):
+    last_digit = i ** 2 % 10
+    if last_digit == 2 or last_digit == 5 or last_digit == 8:
+        total += i
+print(total)
