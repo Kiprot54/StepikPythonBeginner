f42f7dd751afe5e9ef7f5a2fd659e2db62c98487
Kiprot54 <KirKoLearn@gmail.com>
2024-07-29 20:50:27 +0300

    Решил задачу 7.3.8

---
+++ task7.3.8.Alternating_sum.py
@@ -0,0 +1,26 @@
+import ifnumber
+
+print('Знакочередующаяся сумма')
+print()
+
+while True:
+    n = input('Введи целое положительное число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) > 0:
+        n = int(n)
+        print()
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
+    print()
+total = 0
+# for i in range(1, n + 1):
+    # if i % 2 != 0:
+    #     total += i
+    # else:
+    #     total -= i
+if n % 2 != 0:
+    total = n // 2 + 1
+else:
+    total = n // (-2)
+print(f'{total}')
