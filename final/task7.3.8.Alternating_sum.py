032f27b828bf5c26b72564267d0ca2815e9f60ae
Alex Ko <help.mail015@gmail.com>
2024-07-31 11:10:02 +0300

    Update problems

--- final/task7.3.8.Alternating_sum.py
+++ final/task7.3.8.Alternating_sum.py
@@ -1,6 +1,6 @@
 import ifnumber
 
-print('Сумма делителей')
+print('Знакочередующаяся сумма')
 print()
 
 while True:
@@ -14,7 +14,13 @@ while True:
         print('Данные введены некорректно! Нужно ввести целое положительное число')
     print()
 total = 0
-for i in range(1, n + 1):
-    if n % i == 0:
-        total += i
-print(f'Сумма всех делителей числа {n} равна {total}')
+# for i in range(1, n + 1):
+    # if i % 2 != 0:
+    #     total += i
+    # else:
+    #     total -= i
+if n % 2 != 0:
+    total = n // 2 + 1
+else:
+    total = n // (-2)
+print(f'Знакочередующуюся сумма от 1 до {n} равна {total}')
