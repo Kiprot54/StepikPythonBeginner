2694b29ef55b2219a0aa09d8a23a55bbe77d8d60
Kiprot54 <KirKoLearn@gmail.com>
2024-07-31 18:56:35 +0300

    Решил задачу 7.4.5

---
+++ task7.4.5.Number_of_fives.py
@@ -0,0 +1,23 @@
+import ifnumber
+
+print('Количество пятерок')
+print()
+
+counter = 0
+while True:
+    while True:
+        n = input('Введи целое число: ')
+        if_number = ifnumber.if_number(n)
+        if if_number == 'int':
+            n = int(n)
+            print()
+            break
+        else:
+            print('Данные введены некорректно! Нужно ввести целое число')
+        print()
+    if 1 <= n <= 5:
+        if n == 5:
+            counter += 1
+    else:
+        break
+print(counter)
