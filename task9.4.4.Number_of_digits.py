130c965dc2aca85bf3890d5dcd32f35ef2bf80c1
Kiprot54 <KirKoLearn@gmail.com>
2024-08-27 14:52:06 +0300

    Решил задачу 9.4.4

---
+++ task9.4.4.Number_of_digits.py
@@ -0,0 +1,16 @@
+print('Количество цифр')
+print()
+
+while True:
+    s = input('Введи строку: ')
+    if len(s.strip()) == 0:
+        print('Строка не должна быть пустой')
+        print()
+    else:
+        break
+print()
+counter = 0
+for c in s:
+    if c.isdigit():
+        counter += 1
+print(f'В строке "{s}" {counter} цифр')
