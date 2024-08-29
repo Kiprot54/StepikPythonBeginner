d7ffb2f9e6888199f1b9457e995ca1e96866e005
Kiprot54 <KirKoLearn@gmail.com>
2024-08-29 20:15:05 +0300

    Решил задачу 9.5.2

---
+++ task9.5.2.Car_number.py
@@ -0,0 +1,26 @@
+print('Автомобильный номер')
+print()
+
+while True:
+    s = input('Введи автомобильный номер: ')
+    if len(s) == 0:
+        print('Строка не должна быть пустой')
+        print()
+    else:
+        break
+print()
+temp = ''
+all_digits = '0123456789'
+all_letters = 'АВЕКМНОРСТУХ'
+if 8 <= len(s) <= 10:
+    letters = s[0] + s[4:6]
+    digits = s[1:4] + s[7:]
+    separator = s[6]
+    if digits.isdigit() and separator == '_':
+        temp = ''
+    for c in letters:
+        if c not in all_letters:
+            temp = 'не'
+else:
+    temp = 'не'
+print(f'Автомобильный номер {s} {temp}корректный')
