8fe9049de307fc2fa6565bec995ae10c236ca829
Kiprot54 <KirKoLearn@gmail.com>
2024-09-22 20:58:13 +0300

    Решил задачу 10.2.9

---
+++ task10.2.9.Delete_me_completely.py
@@ -0,0 +1,13 @@
+print('Удали меня полностью')
+print()
+
+while True:
+    s = input('Введи строку: ')
+    if len(s.strip()) == 0:
+        print('Строка не должна быть пустой')
+        print()
+    else:
+        break
+print()
+
+print(s.replace('@', ''))
