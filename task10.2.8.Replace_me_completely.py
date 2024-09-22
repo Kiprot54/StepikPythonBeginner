a1edd0578a2d2d48d642ab54d19afc99012ea696
Kiprot54 <KirKoLearn@gmail.com>
2024-09-22 20:56:05 +0300

    Решил задачу 10.2.8

---
+++ task10.2.8.Replace_me_completely.py
@@ -0,0 +1,13 @@
+print('Замени меня полностью')
+print()
+
+while True:
+    s = input('Введи строку: ')
+    if len(s.strip()) == 0:
+        print('Строка не должна быть пустой')
+        print()
+    else:
+        break
+print()
+
+print(s.replace('1', 'one'))
