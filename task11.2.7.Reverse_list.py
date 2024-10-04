92a41af1f94940140c4b039ab0d914a24737d2fb
Kiprot54 <KirKoLearn@gmail.com>
2024-10-04 19:50:07 +0300

    Решил задачу 11.2.7

---
+++ task11.2.7.Reverse_list.py
@@ -0,0 +1,6 @@
+print('Разворот списка')
+print()
+
+languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
+languages = languages[::-1]
+print(languages)
