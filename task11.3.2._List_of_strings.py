5a330fa04043d6e7d6952c9f8d120d37a374c440
Kiprot54 <KirKoLearn@gmail.com>
2024-10-08 20:21:02 +0300

    Решил задачу 11.3.2

---
+++ task11.3.2._List_of_strings.py
@@ -0,0 +1,29 @@
+import ifnumber
+
+print('Список строк')
+print()
+
+while True:
+    n = input('Введи целое положительное число: ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) > 0:
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести число')
+        print()
+
+lst = []
+print()
+for i in range(1, n + 1):
+    while True:
+        s = input(f'Введи {i}-ю строку: ')
+        if len(s.strip()) == 0:
+            print('Строка не должна быть пустой')
+            print()
+        else:
+            break
+    print()
+    lst.append(s)
+print(lst)
+
