910afc2d27c76f3904831879b0a997d4d5a0a86a
Kiprot54 <KirKoLearn@gmail.com>
2024-10-29 18:07:15 +0200

    Решил задачу 11.4.4

---
+++ task11.4.4.No_duplicates.py
@@ -0,0 +1,29 @@
+import ifnumber
+
+print('Без дубликатов')
+print()
+
+while True:
+    n = input('Сколько строк будет введено? ')
+    if_number = ifnumber.if_number(n)
+    if if_number == 'int' and int(n) > 0:
+        n = int(n)
+        break
+    else:
+        print('Данные введены некорректно! Нужно ввести целое положительное число')
+        print()
+print()
+lst = []
+for i in range(1, n + 1):
+    while True:
+        s = input(f'Введи {i}-ю строку: ')
+        if len(s.strip()) == 0:
+            print('Строка не должна быть пустой')
+            print()
+        else:
+            break
+    print()
+
+    if s not in lst:
+        lst.append(s)
+print(*lst, sep='\n')
