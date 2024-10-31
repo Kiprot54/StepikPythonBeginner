6cf390dcd417cb922b7ecea72f30f54dc230a5d1
Kiprot54 <KirKoLearn@gmail.com>
2024-10-31 19:33:01 +0200

    Решил задачу 11.4.7

--- task11.4.7.Negatives_Zeros_and_Positives.py
+++ task11.4.7.Negatives_Zeros_and_Positives.py
@@ -16,6 +16,7 @@ print()
 positive_numbers = []
 negative_numbers = []
 zero_numbers = []
+numbers = []
 for i in range(1, n + 1):
     while True:
         num = input(f'Введи {i}-е целое число: ')
@@ -26,10 +27,22 @@ for i in range(1, n + 1):
         else:
             print('Данные введены некорректно! Нужно ввести целое число')
             print()
+    numbers.append(num)
     if num > 0:
         positive_numbers.append(num)
     elif num < 0:
         negative_numbers.append(num)
     else:
         zero_numbers.append(num)
-print(*negative_numbers, *zero_numbers, *positive_numbers, sep='\n')
+
+print()
+print(f'Среди введённых чисел', *numbers)
+print()
+print('отрицательные числа:')
+print(*negative_numbers, sep='\n')
+print()
+print('нули:')
+print(*zero_numbers, sep='\n')
+print()
+print(f'положительные числа:')
+print(*positive_numbers, sep='\n')
