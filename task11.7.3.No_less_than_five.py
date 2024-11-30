246b1770ba3f30663b7af0c2e8b860f4b474faf5
Kiprot54 <KirKoLearn@gmail.com>
2024-11-30 19:33:09 +0200

    Решил задачу 11.7.4

--- task11.7.3.No_less_than_five.py
+++ task11.7.3.No_less_than_five.py
@@ -1,3 +1,6 @@
+print('Не меньше пяти')
+print()
+
 keywords = ['False', 'True', 'None', 'and', 'with', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'try', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'while', 'yield']
 
 print([el for el in keywords if len(el) >= 5])
