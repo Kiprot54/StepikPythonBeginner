15209df5de14b82066fbe7bbe9601638a39fcd9f
Kiprot54 <KirKoLearn@gmail.com>
2024-12-24 19:27:43 +0200

    Решил задачу 12.2.4

--- task12.2.3.Sum_of_numbers.py
+++ task12.2.3.Sum_of_numbers.py
@@ -1,16 +1,33 @@
+import ifnumber
+
 print('Сумма чисел')
 print()
 
+numbers = []
 while True:
-    numbers = input('Введи строку, содержащую натуральные числа: ')
-    if len(numbers.strip()) == 0 and int(numbers) > 0:
+    s = input('Введи строку, содержащую целые числа не меньше нуля: ').strip()
+    if len(s) == 0:
         print('Строка не должна быть пустой')
         print()
     else:
-        break
+        numbers = s.split()
+        flag = True
+        for el in numbers:
+            if ifnumber.if_number(el) != 'int':
+                print('Нужно ввести строку, содержащую целые числа не меньше нуля')
+                flag = False
+                break
+            else:
+                if int(el) < 0:
+                    print('Нужно ввести строку, содержащую целые числа не меньше нуля')
+                    flag = False
+                    break
+        if flag:
+            break
+
 print()
 total = 0
-numbers = numbers.split()
+numbers = s.split()
 for el in numbers:
     total += int(el)
 print(*numbers, sep='+', end='')
