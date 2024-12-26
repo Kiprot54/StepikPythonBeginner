dcc5da86cd0d6c50886447aba8f5ded5ad743abf
Kiprot54 <KirKoLearn@gmail.com>
2024-12-26 18:13:48 +0200

    Решил задачу 12.2.5

---
+++ task12.2.5.Longest.py
@@ -0,0 +1,13 @@
+print('Самый длинный')
+print()
+
+while True:
+    s = input('Введи строку: ')
+    if len(s.strip()) == 0:
+        print('Строка не должна быть пустой')
+        print()
+    else:
+        break
+print()
+
+print(max([len(el) for el in s.split()]))
