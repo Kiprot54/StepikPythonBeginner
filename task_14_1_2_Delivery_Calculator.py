5776a023c15612855b787b202f95cb1933a48c39
Kiprot54 <KirKoLearn@gmail.com>
2025-03-04 22:40:42 +0200

    Решил задачу 14.1.2

---
+++ task_14_1_2_Delivery_Calculator.py
@@ -0,0 +1,26 @@
+import ifnumber
+
+print('Калькулятор доставки')
+print()
+
+def get_shipping_cost(quantity):
+    num = 1000
+    if quantity != 1:
+        for i in range(2, quantity + 1):
+            num += 1
+    return num
+
+def get_num():
+    while True:
+        num = input('Введи целое положительное число: ')
+        if_number = ifnumber.if_number(num)
+        if if_number == 'int' and int(num):
+            num = int(num)
+            return num
+        else:
+            print('Данные введены некорректно! Нужно ввести целое положительное число')
+            print()
+
+n = get_num()
+
+print(get_shipping_cost(n))
