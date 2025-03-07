1a94ec4a8dc69ffb063bf81498424849e2afd4e3
Kiprot54 <KirKoLearn@gmail.com>
2025-03-07 20:38:04 +0200

    Решил задачу 14.1.6

---
+++ task_14_1_6_Magical_dates.py
@@ -0,0 +1,26 @@
+print('Магические даты')
+print()
+
+def is_magic(date):
+    numbers = date.split('.')
+    num = int(numbers[0])
+    month = int(numbers[1])
+    year = int(numbers[2])
+    return num * month == year % 100
+
+def get_date():
+    while True:
+        s = input('Введи строковое представление корректной даты: ')
+        if len(s.strip()) == 0:
+            print('Строка не должна быть пустой')
+            print()
+        else:
+            lst = s.split('.')
+            if lst[0].isdigit() or lst[1].isdigit() or lst[2].isdigit():
+                print('Нужно ввести строковое представление корректной даты')
+            else:
+                return s
+
+s = get_date()
+
+print(is_magic(s))
