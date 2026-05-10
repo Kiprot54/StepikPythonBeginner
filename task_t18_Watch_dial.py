d7736b2622e06518a1c3d126671f9df9fa40629d
Kiprot54 <KirKoLearn@gmail.com>
2026-05-10 20:35:36 +0300

    Задача turtle18 - Добавил try/except

--- task_t18_Watch_dial.py
+++ task_t18_Watch_dial.py
@@ -13,55 +13,67 @@ def turtle_line(side):
     t.backward(side)
 
 def get_bg_color():
-    color = ""
+    hex_color = '0123456789abcdef'
     while True:
-        color = input('Введи цвет фона: ')
-        len_color = len(color) == 4 or len(color) == 7
-        hex_color = '0123456789abcdef'
-        is_correct_hex = True
+        try:
+            color = input('Введи цвет фона: ')
 
-        for i in range(1, len(color)):
-            if color[i] not in hex_color:
-                is_correct_hex = False
-                break
+            if color[0] != '#':
+                raise ValueError('Нет символа #')
 
+            if len(color) not in (4, 7):
+                raise ValueError('Неверная длина')
 
-        if color[0] != '#' or not len_color or not is_correct_hex:
+            for char in color[1:]:
+                if char.lower() not in hex_color:
+                    raise ValueError('Не HEX формат')
+
+            return color
+
+        except ValueError:
             print('Цвет введён неправильно')
-            continue
-        break
-    return color
+
+        except IndexError:
+            print('Пустая строка')
 
 def get_side():
     while True:
-        side = input('Введи расстояние от центра: ')
-        if not side.isdigit():
+        try:
+            side = input('Введи расстояние от центра: ')
+            if not side.isdigit():
+                raise ValueError('Нужно ввести число')
+            return int(side)
+        except ValueError:
             print('Нужно ввести число')
-            continue
-        return int(side)
+        except IndexError:
+            print('Пустая строка')
 
 
 def get_shape(target):
-    shape = ''
     shape_list = ['turtle', 'arrow', 'triangle']
     while True:
-        shape = input(f'Введи рисунок {target} (arrow, turtle, triangle): ')
-        if shape not in shape_list:
-            print('Неверный ввод')
-            continue
-        break
-    return shape
+        try:
+            shape = input(f'Введи рисунок {target} (arrow, turtle, triangle): ')
+            if shape not in shape_list:
+                raise ValueError('Shape не из списка')
+            return shape
+        except ValueError:
+            print('Нужно ввести рисунок из списка')
+        except IndexError:
+            print('Пустая строка')
+
 
 def get_time():
     while True:
-        time = input('Введи время на часах от 1 до 12: ')
-        if not time.isdigit():
+        try:
+            time = input('Введи время на часах от 1 до 12: ')
+            if not time.isdigit() or not 1 <= int(time) <= 12:
+                raise ValueError('Время введено неправильно')
+            return int(time)
+        except ValueError:
             print('Время введено неправильно')
-            continue
-        elif not 1 <= int(time) <= 12:
-            print('Время введено неправильно')
-            continue
-        return int(time)
+        except IndexError:
+            print('Пустая строка')
 
 def turtle_circle(side, watch, arrow, time):
     t.shape(arrow)
