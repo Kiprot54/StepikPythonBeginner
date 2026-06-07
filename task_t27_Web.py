71a811c9fc9b783db97b2d1d6bd59db435cf4d17
Kiprot54 <KirKoLearn@gmail.com>
2026-06-07 22:54:51 +0300

    Turtle - Сделал задачи t28

--- task_t27_Web.py
+++ task_t27_Web.py
@@ -7,9 +7,10 @@ from turtle_functions import line, run_turtle
 @run_turtle
 def web(n):
     side = 100
+    t.shape('turtle')
     for i in range(n):
         t.right(360 / n)
         line(side, True)
 
-n = get_int('Введи количество лапках: ')
+n = get_int('Введи количество лапок: ')
 web(n)
---
+++ task_t28_Spiral.py
@@ -0,0 +1,22 @@
+import turtle as t
+from math import cos, sin, pi
+
+from functions import get_float
+from turtle_functions import run_turtle
+
+@run_turtle
+def spiral(n: float, b: float):
+    angle = 0
+    l = 2 * n * pi
+    while angle < l:
+        r = b * angle
+        x = r * cos(angle)
+        y = r * sin(angle)
+        second_angle = t.towards(x, y)
+        t.setheading(second_angle)
+        t.goto(x, y)
+        angle += 0.1
+
+n = get_float('Введи количество витков спирали: ')
+b = get_float('Введи шаг спираль: ')
+spiral(n, b)
