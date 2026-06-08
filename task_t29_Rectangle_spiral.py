bd61f103897f0946ba657db1bb35378775247899
Kiprot54 <KirKoLearn@gmail.com>
2026-06-08 17:25:24 +0300

    Turtle - Сделал задачу t29

---
+++ task_t29_Rectangle_spiral.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+from functions import get_int, get_float
+from turtle_functions import run_turtle
+
+
+@run_turtle
+def rectangle_spiral(n: int, step: float) -> None:
+    side = step
+    for i in range(n):
+        t.forward(side)
+        t.left(90)
+        side += step
+
+n = get_int('Введи количество элементов спирали: ')
+step = get_float('Введи расстояние между элементами спирали: ')
+rectangle_spiral(n, step)
