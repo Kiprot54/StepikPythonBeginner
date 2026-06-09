d91e56a6a20f2d672db2596478dc1fccc5886c3e
Kiprot54 <KirKoLearn@gmail.com>
2026-06-09 17:09:00 +0300

    Turtle - Сделал задачи t30 и t31

---
+++ task_t30_Regular_polygons.py
@@ -0,0 +1,28 @@
+import turtle as t
+
+from functions import get_int
+from turtle_functions import run_turtle, go_to, right_polygon
+from math import sin, pi
+
+
+@run_turtle
+def regular_polygon(n):
+    r = 0
+    step = 20
+
+    for i in range(3, n + 3):
+        r += step
+        go_to(r, 0)
+
+        angles = (i - 2) * 180
+        angle = angles / i
+        rotate_angle = 180 - angle / 2
+        t.left(rotate_angle)
+
+        side = 2 * r * sin(pi / i)
+        right_polygon(i, side)
+
+        t.right(rotate_angle)
+
+n = get_int('Введи количество многоугольников: ')
+regular_polygon(n)
---
+++ task_t31_Flower.py
@@ -0,0 +1,29 @@
+import turtle as t
+
+from functions import get_int
+from turtle_functions import run_turtle
+
+def circles_up_down(r):
+
+    t.circle(r)
+    t.circle(-r)
+
+@run_turtle
+def flower(n):
+    r = 50
+    t.pensize(2)
+    for i in range(n):
+        circles_up_down(r)
+        t.left(180 / n)
+
+
+def main():
+    while True:
+        n = get_int('Введи количество окружностей: ')
+        if n % 2 != 0:
+            print('Надо ввести чётное число')
+        else:
+            break
+    flower(n // 2)
+
+main()
