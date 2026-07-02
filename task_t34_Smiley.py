03f4a1af7241e9d2953295eaee573ff7f59e2f62
Kiprot54 <KirKoLearn@gmail.com>
2026-07-02 22:12:51 +0300

    Turtle - Закончил задачу t34

--- task_t34_Smiley.py
+++ task_t34_Smiley.py
@@ -1,34 +1,55 @@
 import turtle as t
 
+from lib.functions import get_color, get_float
 from lib.turtle_functions import run_turtle, go_to
 
+def circle(r, isAngle = False):
+    t.circle(r, 180 if isAngle else 360)
+
+def get_colors(text):
+    colors = get_color(f'° Введи цвет {text} для смайлика в формате HEX: ')
+    return colors
 
 @run_turtle
-def smiley():
-    t.fillcolor('yellow')
+def smiley(size, color_head, color_eyes, color_nose, color_mouth):
+
+    # голова
+    t.fillcolor(color_head)
     t.begin_fill()
-    t.circle(100)
+    circle(size)
     t.end_fill()
 
-    go_to(-50, 130)
-    t.fillcolor('blue')
+    # глаза
+    go_to(-size * 0.5, size * 1.3)
+    t.fillcolor(color_eyes)
     t.begin_fill()
-    t.circle(15)
+    circle(size * 0.15)
     t.end_fill()
 
-    go_to(50, 130)
+    go_to(size * 0.5, size * 1.3)
     t.begin_fill()
-    t.circle(15)
+    circle(size * 0.15)
     t.end_fill()
 
-    go_to(0, 115)
-    t.pensize(10)
+    # нос
+    go_to(0, size * 1.15)
+    t.color(color_nose)
+    t.pensize(size * 0.1)
     t.right(90)
-    t.forward(30)
+    t.forward(size * 0.3)
 
-    go_to(-50, 60)
-    t.color('red')
-    t.circle(50, 180)
+    # рот
+    go_to(-size * 0.5, size * 0.6)
+    t.color(color_mouth)
+    circle(size * 0.5, True)
 
+def main():
+    size = get_float('Введите размер смайлика: ')
+    head = get_colors('головы')
+    eyes = get_colors('глаз')
+    nose = get_colors('носа')
+    mouth = get_colors('рты')
 
-smiley()
+    smiley(size, head, eyes, nose, mouth)
+
+main()
